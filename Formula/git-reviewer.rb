4ae7a8a5801fc57a45c257dd0fc59538ba21ec30
baochuquan <baocq@kanyun.com>
2024-05-07 15:44:34 +0800

    feat: 优化安装逻辑

--- Formula/git-reviewer.rb
+++ Formula/git-reviewer.rb
@@ -4,11 +4,11 @@ class GitReviewer < Formula
   url "https://rubygems.org/downloads/git-reviewer-0.1.0.gem"
   sha256 "3711d1aa8edcbdcae7c909322dbbcbcdd9f63262c3e7bfd2e01e4efc6d6ecef1"
 
-  depends_on "ruby" => :recommended
+  depends_on "ruby"
 
   def install
-    system "gem", "install", "git-reviewer-0.1.0.gem"
-    bin.install_symlink libexec/"git-reviewer"
+    system "gem", "install", "git-reviewer", "-v", "0.1.0", "--install-dir", libexec
+    bin.install_symlink libexec/"bin/git-reviewer"
   end
 
   test do
