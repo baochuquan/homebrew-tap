e51f178c138ebbfa9c47c20092b55000c243bc1b
baochuquan <baocq@kanyun.com>
2024-05-08 20:23:38 +0800

    feat: 优化 formula 逻辑

--- Formula/git-reviewer.rb
+++ Formula/git-reviewer.rb
@@ -10,8 +10,12 @@ class GitReviewer < Formula
     ENV["GEM_HOME"] = libexec
     system "gem", "build", "git-reviewer.gemspec"
     system "gem", "install", "git-reviewer-#{version}.gem"
-    bin.install libexec/"bin/git-reviewer"
-    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+
+    # 将 bin 目录下的可执行文件安装到 Formula 的 bin 目录下
+     bin.install libexec/"bin/git-reviewer"
+
+     # 设置环境变量，以确保 gem 可以在非默认 GEM_HOME 下被找到和执行
+     bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"], GEM_PATH: ENV["GEM_HOME"])
   end
 
   test do
