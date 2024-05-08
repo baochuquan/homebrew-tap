6432144416e9df3a3bb0f83f07cdfe6d25892368
baochuquan <baocq@kanyun.com>
2024-05-08 20:01:52 +0800

    feat: 支持 git-reviewer 0.4.0

--- Formula/git-reviewer.rb
+++ Formula/git-reviewer.rb
@@ -1,8 +1,8 @@
 class GitReviewer < Formula
   desc "git plugin for code review analyze"
   homepage "https://github.com/baochuquan/git-reviewer"
-  url "https://github.com/baochuquan/git-reviewer/archive/refs/tags/0.3.0.tar.gz"
-  sha256 "05e7690fc67730a395cddf4f6a106773e12c9b4e0f9f6401519c63e3a1530f28"
+  url "https://github.com/baochuquan/git-reviewer/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "499367cd60754176e5e5a86e60e7bd19276a94bc8bceaad19ec2478f41775e48"
 
   depends_on "ruby"
 
