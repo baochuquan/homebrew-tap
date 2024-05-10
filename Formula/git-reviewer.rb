e2f90491309308d9c15476a11b676c1ce724a972
baochuquan <baocq@kanyun.com>
2024-05-10 17:49:47 +0800

    feat: 升级至 0.8.0

--- Formula/git-reviewer.rb
+++ Formula/git-reviewer.rb
@@ -1,8 +1,8 @@
 class GitReviewer < Formula
   desc "git plugin for code review analyze"
   homepage "https://github.com/baochuquan/git-reviewer"
-  url "https://github.com/baochuquan/git-reviewer/archive/refs/tags/0.7.0.tar.gz"
-  sha256 "e6ad2a0f4766011cd059196cad03930a5ec1b290ad79283796f28815d20eaa03"
+  url "https://github.com/baochuquan/git-reviewer/archive/refs/tags/0.8.0.tar.gz"
+  sha256 "5527464507802aced412acbb7fa289b453ed18606a4da2e2cf16938f585477a3"
 
   depends_on "ruby"
 
@@ -11,25 +11,9 @@ class GitReviewer < Formula
     ENV["GEM_HOME"] = libexec
 
     system "gem", "build", "git-reviewer.gemspec"
-    system "gem", "install", "git-reviewer", "-v", "0.7.0", "--install-dir", "#{libexec}"
+    system "gem", "install", "git-reviewer", "-v", "0.8.0", "--install-dir", "#{libexec}"
     (bin/"git-reviewer").write_env_script libexec/"bin/git-reviewer", GEM_HOME: ENV["GEM_HOME"]
 
-    # # 安装gem到libexec目录
-    # system "gem", "install", "git-reviewer", "-v", "0.4.0", "--install-dir", "#{libexec}"
-    # # 创建环境脚本指向gem bin目录下的执行文件
-    # bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
-
-
-
-    # ENV["GEM_HOME"] = libexec
-    # system "gem", "build", "git-reviewer.gemspec"
-    # system "gem", "install", "git-reviewer-#{version}.gem"
-
-    # # 将 bin 目录下的可执行文件安装到 Formula 的 bin 目录下
-    #  bin.install libexec/"bin/git-reviewer"
-
-    #  # 设置环境变量，以确保 gem 可以在非默认 GEM_HOME 下被找到和执行
-    #  bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"], GEM_PATH: ENV["GEM_HOME"])
   end
 
   test do
