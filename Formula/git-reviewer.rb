dd67ecab1efb7e7c8c1e80ad2c5a38a65c48de36
baochuquan <baocq@kanyun.com>
2024-05-11 11:10:42 +0800

    chore: 更新至 0.9.0

--- Formula/git-reviewer.rb
+++ Formula/git-reviewer.rb
@@ -1,19 +1,31 @@
 class GitReviewer < Formula
   desc "git plugin for code review analyze"
   homepage "https://github.com/baochuquan/git-reviewer"
-  url "https://github.com/baochuquan/git-reviewer/archive/refs/tags/0.8.0.tar.gz"
-  sha256 "5527464507802aced412acbb7fa289b453ed18606a4da2e2cf16938f585477a3"
+  url "https://github.com/baochuquan/git-reviewer/archive/refs/tags/0.9.0.tar.gz"
+  sha256 "5b278376fc8c66c666fd62150f38997ba65f6ea78a1d1dacbcfcf93aa07f34e8"
 
   depends_on "ruby"
 
+  resource "claide" do
+    url "https://rubygems.org/downloads/claide-1.0.3.gem"
+    sha256 "1fbc84bad66bbde2058d13875f73a25fd1d252fa1620837563cf11a1abee5a4d"
+  end
+
+  resource "terminal-table" do
+    url "https://rubygems.org/downloads/terminal-table-3.0.2.gem"
+    sha256 "f951b6af5f3e00203fb290a669e0a85c5dd5b051b3b023392ccfd67ba5abae91"
+  end
+
   def install
-
     ENV["GEM_HOME"] = libexec
-
+    resources.each do |r|
+      r.fetch
+      system "gem", "install", r.cached_download, "--ignore-dependencies",
+             "--no-document", "--install-dir", libexec
+    end
     system "gem", "build", "git-reviewer.gemspec"
-    system "gem", "install", "git-reviewer", "-v", "0.8.0", "--install-dir", "#{libexec}"
+    system "gem", "install", "--ignore-dependencies", "git-reviewer-#{version}.gem"
     (bin/"git-reviewer").write_env_script libexec/"bin/git-reviewer", GEM_HOME: ENV["GEM_HOME"]
-
   end
 
   test do
